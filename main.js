74936327ceb46adf0106ed55475df6fb3dc28296
Devarshi Patel <devarshi.ap@gmail.com>
2021-07-19 20:21:30 -0400

    Childish Gambino kinda cool

--- main.js
+++ main.js
@@ -23,35 +23,95 @@ function getResults(query) {
 }
 
 function displayResults (weather) {
+
+    // debugging purposes
+
     console.log(weather);
+
+    // Locations
+
     let city = document.querySelector('.city');
     city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+    // Date (formated)
 
     let now = new Date();
     let date = document.querySelector('.date');
     date.innerText = dateFormatter(now);
 
+    // Temperature
+
     let temp = document.querySelector('.temp');
     temp.innerHTML = `${Math.round(weather.main.temp)}&#176;c`;
+
+    // Temperature Feels Like
+
+    let feelsLike = document.querySelector('.feels-like');
+    feelsLike.innerHTML = `${Math.round(weather.main.feels_like)}&#176;c`;
     
+    // Weather Condition
+
     let weatherCondition = document.querySelector('.weather');
     weatherCondition.innerText = weather.weather[0].description;
 
-    let high = document.querySelector('.high');
-    high.innerHTML = `${weather.main.temp_max.toFixed(1)}&#176;c`;
+    // Weather Condition Icon
 
-    let low = document.querySelector('.low');
-    low.innerHTML = `${weather.main.temp_min.toFixed(1)}&#176;c`;
+    let weatherParent = document.querySelector('.weather-box');
+    let weatherImgTag = document.querySelector('.weather-icon');
+    let attributes = {
+        'src': getIconUrl(weather.weather[0].icon),
+        'alt': 'weather icon'
+    }
+    setMultipleAttr(weatherImgTag, attributes);
+    weatherParent.append(weatherImgTag);
+
+    // Daily Temperature High/Low
+
+    let high = document.querySelector('.high-low');
+    high.innerHTML = `${Math.round(weather.main.temp_max)}&#176;/${Math.round(weather.main.temp_min)}&#176;`;
+
+    // Humidity %
+
+    let humidity = document.querySelector('.humidity');
+    humidity.innerText = `${weather.main.humidity}%`;
+
+    // Wind Velocity
+
+    let wind = document.querySelector('.wind-velocity');
+    let windDirection = degToCompass(weather.wind.deg)
+    wind.innerHTML = `${weather.wind.speed}m/s ${windDirection}`;
 }
 
 function dateFormatter(d) {
     return d.toLocaleString('en-US', {
-        weekday: 'short', // long, short, narrow
-        day: 'numeric', // numeric, 2-digit
-        year: 'numeric', // numeric, 2-digit
-        month: 'long', // numeric, 2-digit, long, short, narrow
-        hour: 'numeric', // numeric, 2-digit
-        minute: 'numeric', // numeric, 2-digit
-        second: 'numeric', // numeric, 2-digit
+        // comment out whichever values to skip over in the date format
+        weekday: 'short',       // long, short, narrow
+        day: 'numeric',         // numeric, 2-digit
+        year: 'numeric',        // numeric, 2-digit
+        month: 'long',          // numeric, 2-digit, long, short, narrow
+        hour: 'numeric',        // numeric, 2-digit
+        minute: 'numeric',      // numeric, 2-digit
+        // second: 'numeric',   // numeric, 2-digit
     });
+}
+
+function degToCompass(deg) {
+    var num = parseInt((deg/22.5) + 0.5);
+    var directions = [
+        "N", "NNE", "NE", "ENE",
+        "E", "ESE", "SE", "SSE",
+        "S", "SSW", "SW", "WSW",
+        "W", "WNW", "NW", "NNW"
+    ];
+    return directions[(num%16)];
+}
+
+function getIconUrl(iconCode) {
+    return `http://openweathermap.org/img/wn/${iconCode}@2x.png`;
+}
+
+function setMultipleAttr(el, attrs) {
+    for(var key in attrs) {
+        el.setAttribute(key, attrs[key]);
+    }
 }
