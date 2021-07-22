522f2925dd2930184d6a153a1ecdbac69c3634d9
Devarshi Patel <devarshi.ap@gmail.com>
2021-07-21 20:46:57 -0400

    upload version two

--- main.js
+++ main.js
@@ -8,6 +8,25 @@ const api = {
 const searchBox = document.querySelector(".search-box");
 searchBox.addEventListener("keypress", setQuery);
 
+const checkbox = document.getElementById('toggle');
+if(checkbox){
+  checkbox.addEventListener('change', function() {
+      const videoEl = document.getElementById('videoEl');
+      const titleEl = document.getElementById('titleEl');
+
+      if (this.checked) {
+          titleEl.style.color= 'silver';
+          document.getElementById('title').style.opacity= 1;
+          videoEl.setAttribute('src', 'assets/mp4/space.mp4');
+      }
+      if (!(this.checked)) {
+          titleEl.style.color= 'white';
+          videoEl.setAttribute('src', 'assets/mp4/mountain.mp4');
+      }
+  });
+    
+}
+
 function setQuery(evt) {
   // keyCode of 'enter' button is 13
   if (evt.keyCode === 13) {
@@ -40,99 +59,99 @@ function displayResults(weather) {
   const wind = document.querySelector(".wind-velocity");
   
   if (weather.message === "city not found") {
-    console.log("City Not Found");
-
-    // Error Displays
-
-    city.innerText = `City Not Found...`;
-    date.innerText = '</>';
-    temp.innerHTML = `--&#176;c`;
-    feelsLike.innerHTML = `--&#176;c`;
-    weatherCondition.innerText = `none`;
-    high.innerHTML = `--/--`;
-    humidity.innerText = `--%`;
-    wind.innerHTML = `--m/s X`;
+      console.log("City Not Found");
+  
+      // Error Displays
+  
+      city.innerText = `City Not Found...`;
+      date.innerText = '</>';
+      temp.innerHTML = `--&#176;c`;
+      feelsLike.innerHTML = `--&#176;c`;
+      weatherCondition.innerText = `none`;
+      high.innerHTML = `--/--`;
+      humidity.innerText = `--%`;
+      wind.innerHTML = `--m/s X`;
     
   } else {
 
-    // Locations
-
-    city.innerHTML = `${weather.name}, ${weather.sys.country} <span>${getFlagEmoji(weather.sys.country)}</span>`;
-
-    // Date (formated)
-
-    date.innerText = dateFormatter(now);
-
-    // Temperature
-
-    temp.innerHTML = `${Math.round(weather.main.temp)}&#176;c`;
-
-    // Temperature Feels Like
-
-    feelsLike.innerHTML = `${Math.round(weather.main.feels_like)}&#176;c`;
-
-    // Weather Condition
-
-    weatherCondition.innerText = weather.weather[0].description;
-
-    // Weather Condition Icon
-
-    const weatherParent = document.querySelector(".weather-box");
-    const weatherImgTag = document.querySelector(".weather-icon");
-    const attributes = {
-      src: getIconUrl(weather.weather[0].icon),
-      alt: "weather icon",
-    };
-    setMultipleAttr(weatherImgTag, attributes);
-    weatherParent.append(weatherImgTag);
-
-    // Daily Temperature High/Low
-
-    high.innerHTML = `${Math.round(weather.main.temp_max)}&#176;/${Math.round(weather.main.temp_min)}&#176;`;
-
-    // Humidity %
-
-    humidity.innerText = `${weather.main.humidity}%`;
-
-    // Wind Velocity
-
-    const windDirection = degToCompass(weather.wind.deg);
-    wind.innerHTML = `${weather.wind.speed}m/s ${windDirection}`;
+      // Locations
+  
+      city.innerHTML = `${weather.name}, ${weather.sys.country} <span>${getFlagEmoji(weather.sys.country)}</span>`;
+  
+      // Date (formated)
+  
+      date.innerText = dateFormatter(now);
+  
+      // Temperature
+  
+      temp.innerHTML = `${Math.round(weather.main.temp)}&#176;c`;
+  
+      // Temperature Feels Like
+  
+      feelsLike.innerHTML = `${Math.round(weather.main.feels_like)}&#176;c`;
+  
+      // Weather Condition
+  
+      weatherCondition.innerText = weather.weather[0].description;
+  
+      // Weather Condition Icon
+  
+      // const weatherParent = document.querySelector(".weather-box");
+      // const weatherImgTag = document.querySelector(".weather-icon");
+      // const attributes = {
+      //     src: getIconUrl(weather.weather[0].icon),
+      //     alt: "weather icon",
+      // };
+      // setMultipleAttr(weatherImgTag, attributes);
+      // weatherParent.append(weatherImgTag);
+  
+      // Daily Temperature High/Low
+  
+      high.innerHTML = `${Math.round(weather.main.temp_max)}&#176;/${Math.round(weather.main.temp_min)}&#176;`;
+  
+      // Humidity %
+  
+      humidity.innerText = `${weather.main.humidity}%`;
+  
+      // Wind Velocity
+  
+      const windDirection = degToCompass(weather.wind.deg);
+      wind.innerHTML = `${weather.wind.speed}m/s ${windDirection}`;
   }
 }
 
 function dateFormatter(d) {
   return d.toLocaleString("en-US", {
-    // comment out whichever values to skip over in the date format
-    weekday: "short", // long, short, narrow
-    day: "numeric", // numeric, 2-digit
-    year: "numeric", // numeric, 2-digit
-    month: "long", // numeric, 2-digit, long, short, narrow
-    hour: "numeric", // numeric, 2-digit
-    minute: "numeric", // numeric, 2-digit
-    // second: 'numeric',   // numeric, 2-digit
+      // comment out whichever values to skip over in the date format
+      weekday: "short",
+      day: "numeric",
+      year: "numeric",
+      month: "long",
+      hour: "numeric",
+      minute: "numeric", 
+      // second: 'numeric',   
   });
 }
 
 function degToCompass(deg) {
   const num = parseInt(deg / 22.5 + 0.5, 10);
   const directions = [
-    "N",
-    "NNE",
-    "NE",
-    "ENE",
-    "E",
-    "ESE",
-    "SE",
-    "SSE",
-    "S",
-    "SSW",
-    "SW",
-    "WSW",
-    "W",
-    "WNW",
-    "NW",
-    "NNW",
+      "N",
+      "NNE",
+      "NE",
+      "ENE",
+      "E",
+      "ESE",
+      "SE",
+      "SSE",
+      "S",
+      "SSW",
+      "SW",
+      "WSW",
+      "W",
+      "WNW",
+      "NW",
+      "NNW",
   ];
   return directions[num % 16];
 }
@@ -145,14 +164,15 @@ function setMultipleAttr(el, attrs) {
   const keys = Object.keys(attrs);
   const values = Object.values(attrs);
   for (let i = 0; i < keys.length; i += 1) {
-    el.setAttribute(keys[i], values[i]);
+      el.setAttribute(keys[i], values[i]);
   }
 }
 
 function getFlagEmoji(countryCode) {
   const codePoints = countryCode
-    .toUpperCase()
-    .split('')
-    .map(char =>  127397 + char.charCodeAt());
+      .toUpperCase()
+      .split('')
+      .map(char =>  127397 + char.charCodeAt(0));
+
   return String.fromCodePoint(...codePoints);
 }
